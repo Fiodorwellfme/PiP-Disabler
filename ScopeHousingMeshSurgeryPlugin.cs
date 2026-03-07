dc94fe74405ef5c35ea29e7ddb5009ca12c26706
Theodore <39596407+Fiodorwellfme@users.noreply.github.com>
2026-03-07 15:23:32 +0100

    Remove mesh cutting disk cache

--- ScopeHousingMeshSurgeryPlugin.cs
+++ ScopeHousingMeshSurgeryPlugin.cs
@@ -2,7 +2,6 @@ using System;
 using BepInEx;
 using BepInEx.Configuration;
 using EFT.CameraControl;
-using System.IO;
 using UnityEngine;
 
 namespace ScopeHousingMeshSurgery
@@ -20,28 +19,6 @@ namespace ScopeHousingMeshSurgery
         {
             if (Instance != null && VerboseLogging != null && VerboseLogging.Value)
                 Instance.Logger.LogInfo("[V] " + msg);
-        }
-
-        internal static string GetMeshCutCacheDirectory()
-        {
-            string pluginDir;
-            try
-            {
-                pluginDir = Path.GetDirectoryName(Instance != null ? Instance.Info.Location : null);
-            }
-            catch
-            {
-                pluginDir = null;
-            }
-
-            if (string.IsNullOrEmpty(pluginDir))
-                pluginDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "BepInEx", "plugins");
-
-            string cacheDir = Path.Combine(pluginDir, "mesh_cut_cache");
-            if (!Directory.Exists(cacheDir))
-                Directory.CreateDirectory(cacheDir);
-
-            return cacheDir;
         }
 
         /// <summary>
